457ebc96e2e104b89c0d4770b2290fab81397d3d
axstudiosalt <ismailkhanofficialworks@gmail.com>
2024-07-14 19:19:32 +0530

    updated project

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -274,91 +274,93 @@ const Contact = () => {
             <NavBar />
             <div id="contact" className="flex justify-center items-center mt-8 w-full bg-white py-12 lg:py-24 ">
                 <div className="container mx-auto my-8 px-4 lg:px-20" data-aos="zoom-in">
-                    <form name="contact" data-netlify="true" onSubmit={handleSubmit} method="POST">
-                        <input id="test1" type="hidden" name="contact" value="contact" />
-                        <input id="test2" type="hidden" name="subject" value="Sales inquiry from mysitename.netlify.app" />
+                <form name="contact" data-netlify="true" onSubmit={handleSubmit} method="POST">
 
-                        <div className="w-full bg-white p-8 my-4 md:px-12 lg:w-9/12 lg:pl-20 lg:pr-40 mr-auto rounded-2xl shadow-2xl">
-                            <div className="flex">
-                                <h1 className="font-bold text-center lg:text-left text-blue-900 uppercase text-4xl">Send us a message</h1>
-                            </div>
-                            <div className="grid grid-cols-1 gap-5 md:grid-cols-2 mt-5">
-                                <div>
-                                    <input
-                                        name="first_name"
-                                        className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
-                                        type="text"
-                                        placeholder="First Name*"
-                                        value={firstName}
-                                        onChange={(e) => setFirstName(e.target.value)}
-                                        onKeyUp={clearErrors}
-                                        id="firstName"
-                                    />
-                                    {errors.first_name && <p className="text-red-500 text-sm">{errors.first_name}</p>}
-                                </div>
-                                <div>
-                                    <input
-                                        name="last_name"
-                                        className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
-                                        type="text"
-                                        placeholder="Last Name*"
-                                        value={lastName}
-                                        onChange={(e) => setLastName(e.target.value)}
-                                        onKeyUp={clearErrors}
-                                        id="lastName"
-                                    />
-                                    {errors.last_name && <p className="text-red-500 text-sm">{errors.last_name}</p>}
-                                </div>
-                                <div>
-                                    <input
-                                        name="email"
-                                        className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
-                                        type="email"
-                                        placeholder="Email*"
-                                        value={email}
-                                        onChange={(e) => setEmail(e.target.value)}
-                                        onKeyUp={clearErrors}
-                                        id="txtEmail"
-                                    />
-                                    {errors.email && <p className="text-red-500 text-sm">{errors.email}</p>}
-                                </div>
-                                <div>
-                                    <input
-                                        name="phone_number"
-                                        className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
-                                        type="number"
-                                        placeholder="Phone*"
-                                        value={phone}
-                                        onChange={(e) => setPhone(e.target.value)}
-                                        onKeyUp={clearErrors}
-                                        id="txtPhone"
-                                    />
-                                    {errors.phone_number && <p className="text-red-500 text-sm">{errors.phone_number}</p>}
-                                </div>
-                            </div>
-                            <div className="my-4">
-                                <textarea
-                                    name="message"
-                                    placeholder="Message*"
-                                    className="w-full h-32 bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
-                                    value={message}
-                                    onChange={(e) => setMessage(e.target.value)}
-                                    onKeyUp={clearErrors}
-                                    id="txtMsg"
-                                ></textarea>
-                                {errors.message && <p className="text-red-500 text-sm">{errors.message}</p>}
-                            </div>
-                            <div className="my-2 w-1/2 lg:w-2/4">
-                                <button
-                                    type="submit"
-                                    id="submitBtn"
-                                    className="uppercase text-sm font-bold tracking-wide bg-gray-500 hover:bg-blue-900 text-gray-100 p-3 rounded-lg w-full focus:outline-none focus:shadow-outline"
-                                >
-                                    Send Message
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+    <input type="hidden" name="form-name" value="contact" />
+    <input type="hidden" name="subject" value="Sales inquiry from mysitename.netlify.app" />
+
+    <div className="w-full bg-white p-8 my-4 md:px-12 lg:w-9/12 lg:pl-20 lg:pr-40 mr-auto rounded-2xl shadow-2xl">
+        <div className="flex">
+            <h1 className="font-bold text-center lg:text-left text-blue-900 uppercase text-4xl">Send us a message</h1>
+        </div>
+        <div className="grid grid-cols-1 gap-5 md:grid-cols-2 mt-5">
+            <div>
+                <input
+                    name="first_name"
+                    className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                    type="text"
+                    placeholder="First Name*"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                    onKeyUp={clearErrors}
+                    id="firstName"
+                />
+                {errors.first_name && <p className="text-red-500 text-sm">{errors.first_name}</p>}
+            </div>
+            <div>
+                <input
+                    name="last_name"
+                    className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                    type="text"
+                    placeholder="Last Name*"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                    onKeyUp={clearErrors}
+                    id="lastName"
+                />
+                {errors.last_name && <p className="text-red-500 text-sm">{errors.last_name}</p>}
+            </div>
+            <div>
+                <input
+                    name="email"
+                    className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                    type="email"
+                    placeholder="Email*"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    onKeyUp={clearErrors}
+                    id="txtEmail"
+                />
+                {errors.email && <p className="text-red-500 text-sm">{errors.email}</p>}
+            </div>
+            <div>
+                <input
+                    name="phone_number"
+                    className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                    type="number"
+                    placeholder="Phone*"
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                    onKeyUp={clearErrors}
+                    id="txtPhone"
+                />
+                {errors.phone_number && <p className="text-red-500 text-sm">{errors.phone_number}</p>}
+            </div>
+        </div>
+        <div className="my-4">
+            <textarea
+                name="message"
+                placeholder="Message*"
+                className="w-full h-32 bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                onKeyUp={clearErrors}
+                id="txtMsg"
+            ></textarea>
+            {errors.message && <p className="text-red-500 text-sm">{errors.message}</p>}
+        </div>
+        <div className="my-2 w-1/2 lg:w-2/4">
+            <button
+                type="submit"
+                id="submitBtn"
+                className="uppercase text-sm font-bold tracking-wide bg-gray-500 hover:bg-blue-900 text-gray-100 p-3 rounded-lg w-full focus:outline-none focus:shadow-outline"
+            >
+                Send Message
+            </button>
+        </div>
+    </div>
+</form>
+
                     <div className="w-full lg:-mt-96 lg:w-2/6 px-8 py-6 ml-auto bg-blue-900 rounded-2xl">
                         <div className="flex flex-col text-white">
                             <div className="flex my-4 w-2/3 lg:w-3/4">
